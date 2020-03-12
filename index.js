9b2b490abb73bf7e814c693deaf669c955b5dcc8
grovem <matthewg74@gmail.com>
2020-03-11 22:31:44 -0600

    added code to allow user to update roles

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ let departmentChoice;
 let departmentID;
 let departmentName;
 let deletedDepartmentName;
+let salary;
+let role;
+let roleID;
+let updatedRole;
+let updatedTitle;
+let updatedSalary;
+let roleDepartmentID;
+let defaultSalary;
 
 
 var connection = mysql.createConnection({
@@ -21,17 +29,24 @@ connection.connect((err) => {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     getDepartments();
+    getRoles();
     getInfo();
 })
 
 const departments = [];
+
+const departmentIDs = [];
+
+const roles = [];
+
+const salaries = [];
 
 const questions = [
     {
         type: "list",
         name: "action",
         message: "What would you like to do?",
-        choices: ["View all employees", "View employees by department", "View employees by manager", "View departments","Add a department", "Delete a department", "Add a role", "Update a role", "Add an employee", "Update an employee", "EXIT"]
+        choices: ["View all employees", "View employees by department", "View employees by manager", "View departments", "Add a department", "Delete a department", "Add a role", "Update a role", "Add an employee", "Update an employee", "EXIT"]
     },
     {
         type: "list",
@@ -44,7 +59,7 @@ const questions = [
     },
     {
         type: "list",
-        name: "deptChoice",
+        name: "managerChoice",
         message: "Select a manager:",
         choices: departments,
         when: function (answers) {
@@ -78,6 +93,15 @@ const questions = [
     },
     {
         type: "input",
+        name: "roleID",
+        message: "Enter a new role ID:",
+
+        when: function (answers) {
+            return answers.action === "Add a role";
+        }
+    },
+    {
+        type: "input",
         name: "roleTitle",
         message: "Enter a title for the new role:",
 
@@ -94,11 +118,46 @@ const questions = [
         }
     },
     {
-        type: "input",
+        type: "list",
         name: "roleDeptID",
-        message: "Enter a department ID for the new role:",
+        message: "Select a department ID for the new role:",
+        choices: departmentIDs,
         when: function (answers) {
             return answers.action === "Add a role";
+        }
+    },
+    {
+        type: "list",
+        name: "roleUpdate",
+        message: "Select a role:",
+        choices: roles,
+        when: function (answers) {
+            return answers.action === "Update a role";
+        }
+    },
+    {
+        type: "list",
+        name: "roleUpdateChoice",
+        message: "Select an action:",
+        choices: ["Update", "Delete"],
+        when: function (answers) {
+            return answers.action === "Update a role";
+        }
+    },
+    {
+        type: "input",
+        name: "updateTitle",
+        message: "Enter new title:",
+        when: function (answers) {
+            return answers.roleUpdateChoice === "Update";
+        }
+    },
+    {
+        type: "input",
+        name: "updateSalary",
+        message: "Enter new salary:",
+        when: function (answers) {
+            return answers.roleUpdateChoice === "Update";
         }
     },
     {
@@ -135,18 +194,17 @@ const questions = [
     },
 ];
 
-const advance =[
+const advance = [
     {
         type: "confirm",
         name: "continue",
         message: "Would you like to make additional changes/queries?",
     }
 ]
-const data = [];
 
 function advancePrompts() {
     return inquirer.prompt(advance).then(answers => {
-        
+
         if (answers.continue === true) {
             askQuestions();
         } else {
@@ -159,17 +217,19 @@ function askQuestions() {
     return inquirer.prompt(questions).then(answers => {
         departmentName = answers.deptName;
         deletedDepartmentName = answers.deptDeleteName;
-        let role = answers.roleTitle;
-        let salary = answers.roleSalary;
+        role = answers.roleTitle;
+        roleID = answers.roleID;
+        salary = answers.roleSalary;
+        roleDept = answers.roleDept;
+        roleDeptID = answers.roleDeptID;
+        updatedRole = answers.roleUpdate;
+        updatedTitle = answers.updateTitle;
+        updatedSalary = answers.updateSalary;
         departmentID = answers.deptID;
         departmentChoice = answers.deptChoice;
         let firstName = answers.fname;
         let lastName = answers.lname;
-        let roleDepartmentID = answers.roleDeptID;
         let managerID = answers.managerID;
-
-        data.push(answers);
-     
 
         if (answers.action === "View all employees") {
             viewAllEmployees();
@@ -193,11 +253,25 @@ function askQuestions() {
 
         if (answers.action === "Add a department") {
             departments.push(answers.deptName);
+            // departmentIDs.push(answers.deptID);
             addDepartment();
         }
 
         if (answers.action === "Delete a department") {
             deleteDepartment();
+        }
+
+        if (answers.action === "Add a role") {
+            addRole();
+        }
+
+        if (answers.action === "Update a role") {
+            if (answers.roleUpdateChoice === "Update") {
+                updateRole();
+            } else {
+                deleteRole();
+            }
+
         }
 
         if (answers.action === "EXIT") {
@@ -208,13 +282,24 @@ function askQuestions() {
 
 
 async function getDepartments() {
-    connection.query(`SELECT dept_name FROM companydb.departments;`, function (err, res) {
+    connection.query(`SELECT id, dept_name FROM companydb.departments;`, function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
             departments.push(res[i].dept_name);
+            departmentIDs.push(res[i].id);
         }
     })
 };
+
+function getRoles() {
+    connection.query(`SELECT id FROM companydb.roles;`, function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            roles.push(res[i].id);
+        }
+    })
+};
+
 
 function viewAllEmployees() {
     connection.query(`SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.dept_name, roles.salary, managers.manager_name
@@ -258,6 +343,33 @@ function deleteDepartment() {
         advancePrompts();
     });
 }
+
+function addRole() {
+    connection.query(`INSERT INTO roles (id, title, salary, dept_id)
+    VALUES (${roleID}, "${role}", "${salary}", "${roleDeptID}");`, function (err, res) {
+        if (err) throw err;
+        advancePrompts();
+    });
+}
+
+function updateRole() {
+    connection.query(`UPDATE roles
+    SET title = "${updatedTitle}", salary = "${updatedSalary}"
+    WHERE id = "${updatedRole}";`, function (err, res) {
+        if (err) throw err;
+        advancePrompts();
+    });
+}
+
+function deleteRole() {
+    connection.query(`DELETE FROM roles
+    WHERE id = "${updatedRole}";`, function (err, res) {
+        if (err) throw err;
+        advancePrompts();
+    });
+}
+
+
 
 async function getInfo() {
     askQuestions();
