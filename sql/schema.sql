e95ebf03967abc2f38f864ce97d9845c78e17e17
grovem <matthewg74@gmail.com>
2020-03-13 09:55:26 -0600

    added schema.sql

---
+++ sql/schema.sql
@@ -0,0 +1,46 @@
+DROP DATABASE IF EXISTS companyDB;
+
+CREATE DATABASE companyDB;
+
+USE companyDB;
+
+CREATE TABLE departments
+(
+    id INT,
+    dept_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE roles
+(
+    id INT,
+    title VARCHAR(30),
+    salary DECIMAL NOT NULL,
+    dept_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (dept_id) REFERENCES departments (id)
+);
+
+CREATE TABLE managers
+(
+    id INT,
+    mgr_first_name VARCHAR(30) NOT NULL,
+    mgr_last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    full_name VARCHAR(50),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE employees
+(
+    id INT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    full_name VARCHAR(50),
+    PRIMARY KEY(id),
+    FOREIGN KEY (role_id) REFERENCES roles (id),
+    FOREIGN KEY (manager_id) REFERENCES managers (id)
+);
+
